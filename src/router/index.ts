9e2e3af6e594f4fe075602b81eb1e32a87749296
liamjung <liamjung@live.com>
2021-06-20 21:52:29 +0800

    feat: 安装router

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+export const routes = [
+  // TODO 定义路由，每个路由对应一个组件
+  // { path: '', component: },
+];
+
+export default createRouter({
+  history: createWebHistory(),
+  routes,
+});
