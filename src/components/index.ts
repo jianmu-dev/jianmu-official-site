0e50b926098643fe58ec05c5cbbbc644e1e0d46e
liamjung <liamjung@live.com>
2021-06-22 14:08:02 +0800

    feat: dsl-editor

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import { App } from 'vue';
+import JmDslEditor from './dsl-editor/index.vue';
 import { ElCarousel, ElCarouselItem } from 'element-plus';
 // 设置element-plus全局样式
 import 'element-plus/lib/theme-chalk/index.css';
@@ -6,6 +7,8 @@ import 'element-plus/lib/theme-chalk/index.css';
 export default {
   // app.use()触发install的调用
   install: (app: App) => {
+    app.component(JmDslEditor.name, JmDslEditor);
+
     // 按需加载
     app.use(ElCarousel);
     app.use(ElCarouselItem);
