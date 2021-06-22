4ede6d8e88063721cfb3655f1e7bba3ce243e9a8
liamjung <liamjung@live.com>
2021-06-22 14:30:53 +0800

    perf: 优化代码

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 export const routes = [
-  // TODO 定义路由，每个路由对应一个组件
-  // { path: '', component: },
+  { path: '/', component: () => import('@/views/index.vue') },
 ];
 
 export default createRouter({
