919955c53150e21c90b4e4daf90336715af7f999
liamjung <liamjung@live.com>
2021-06-22 14:14:13 +0800

    feat: scrollbar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import { App } from 'vue';
+import JmScrollbar from './scrollbar';
 import JmDslEditor from './dsl-editor/index.vue';
 import { ElCarousel, ElCarouselItem } from 'element-plus';
 // 设置element-plus全局样式
@@ -7,6 +8,7 @@ import 'element-plus/lib/theme-chalk/index.css';
 export default {
   // app.use()触发install的调用
   install: (app: App) => {
+    app.component(JmScrollbar.name, JmScrollbar);
     app.component(JmDslEditor.name, JmDslEditor);
 
     // 按需加载
