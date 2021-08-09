6c5e1c38351058ab17a31b4ed2ef606d73780a49
liamjung <liamjung@live.com>
2021-08-09 18:46:10 +0800

    feat: 配置cdn

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { ConfigEnv, UserConfigExport } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import { name, version } from './package.json';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  }
-})
+export default (
+  { command }: ConfigEnv,
+): UserConfigExport => {
+  return {
+    plugins: [vue()],
+    // base public path
+    base: command === 'build' ? `https://cdn.jianmu.run/${name}/${version}/` : '/',
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
+    },
+  };
+};
