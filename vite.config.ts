396eeb7156838664c256576e9218ac8fa8d21cdc
liamjung <liamjung@live.com>
2021-08-20 16:31:54 +0800

    fix: base

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,12 @@ import { name, version } from './package.json';
 
 // https://vitejs.dev/config/
 export default (
-  { command }: ConfigEnv,
+  { command, mode }: ConfigEnv,
 ): UserConfigExport => {
   return {
     plugins: [vue()],
     // base public path
-    base: command === 'build' ? `https://cdn.jianmu.run/${name}/${version}/` : '/',
+    base: command === 'build' && mode === 'production' ? `https://cdn.jianmu.run/${name}/${version}/` : '/',
     resolve: {
       alias: {
         '@': resolve(__dirname, 'src'),
