242d260c6e97fc428e5f47ff7d478d2876e1c9e8
qbhfh <2682147541@qq.com>
2021-10-12 15:13:12 +0800

    fix: 修改展示节点图标，替换轮播图图片

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue';
+import { createApp, ComponentPublicInstance } from 'vue';
 import App from './app.vue';
 import components from './components';
 import router from './router';
@@ -6,11 +6,20 @@ import router from './router';
 import './global.less';
 // 安装百度统计
 import './utils/baidu-tongji.js';
+import { globalErrorHandler } from './utils/global-error-handler';
 
 const app = createApp(App);
 // 全局注册公共组件
 app.use(components);
 // 注册路由器
 app.use(router);
+
+// 注册全局异常处理方法
+app.config.errorHandler = (err, instance, info) =>
+  globalErrorHandler(err as Error, instance, info, router);
+app.config.globalProperties.$throw = (
+  err: Error,
+  instance: ComponentPublicInstance | null,
+) => globalErrorHandler(err, instance, null, router);
 
 app.mount('#app');
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,3 +3,11 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+// 处理图片格式module ts报错
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.bmp';
+declare module '*.tiff';
