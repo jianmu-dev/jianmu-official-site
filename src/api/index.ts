a9832d2ba86a889de06e317c8d77cbad26f14353
qbhfh <2682147541@qq.com>
2021-11-08 10:03:47 +0800

    fix: 清空h5分支内容

--- src/api/index.ts
+++
@@ -1,5 +0,0 @@
-import rest, { IRequest } from '@/utils/rest';
-
-export async function restProxy<T = any>(request: IRequest): Promise<T> {
-  return await rest(request);
-}
--- src/utils/rest/constants.ts
+++
@@ -1,4 +0,0 @@
-/**
- * 起始页码
- */
-export const START_PAGE_NUM = 1;
