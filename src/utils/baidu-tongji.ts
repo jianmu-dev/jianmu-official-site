514719c0b18841ac802a54332a9a59e064d4c510
liamjung <liamjung@live.com>
2021-11-10 18:17:11 +0800

    refactor: baidu-tongji.ts

--- src/utils/baidu-tongji.ts
+++ src/utils/baidu-tongji.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 if (import.meta.env.MODE === 'cdn') {
   console.debug('Baidu tongji installed');
   // @ts-ignore
@@ -5,7 +6,7 @@ if (import.meta.env.MODE === 'cdn') {
   const _hmt = _hmt || [];
   (function () {
     const hm = document.createElement('script');
-    hm.src = 'https://hm.baidu.com/hm.js?9eb28d9814e01d0d20ea7fe50a7b54bb';
+    hm.src = 'https://hm.baidu.com/hm.js?c5a1703dbc66ac55ad5439d39a96372c';
     const s = document.getElementsByTagName('script')[0];
     // @ts-ignore
     s.parentNode.insertBefore(hm, s);
