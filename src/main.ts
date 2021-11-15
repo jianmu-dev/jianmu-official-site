ba9133ad235a7a3a9865f76ccb3994cabd7b9348
liamjung <liamjung@live.com>
2021-11-15 23:25:38 +0800

    feat: statistics

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import router from './router';
 import './global.less';
 import { globalErrorHandler } from '@/utils/global-error-handler';
 import component from '@/components';
+// 安装百度统计
+import './utils/baidu-tongji.ts';
+
 const app = createApp(App);
 // 全局注册组件
 app.use(component);
