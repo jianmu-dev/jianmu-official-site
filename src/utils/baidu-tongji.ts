cfbd85d1b06dbc0c001e375639b15c906962b96b
liamjung <liamjung@live.com>
2021-11-15 23:18:11 +0800

    fix: statistics

--- src/utils/baidu-tongji.ts
+++ src/utils/baidu-tongji.ts
@@ -1,14 +1,9 @@
-// @ts-ignore
 if (import.meta.env.MODE === 'cdn') {
   console.debug('Baidu tongji installed');
-  // @ts-ignore
-  // eslint-disable-next-line no-use-before-define
-  const _hmt = _hmt || [];
   (function () {
     const hm = document.createElement('script');
     hm.src = 'https://hm.baidu.com/hm.js?c5a1703dbc66ac55ad5439d39a96372c';
     const s = document.getElementsByTagName('script')[0];
-    // @ts-ignore
-    s.parentNode.insertBefore(hm, s);
+    s.parentNode?.insertBefore(hm, s);
   })();
 }
