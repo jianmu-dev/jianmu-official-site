0d437ac5d8c4e2132654ac488b51f7c47da9c149
liamjung <liamjung@live.com>
2021-11-15 22:32:44 +0800

    feat: cdn config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,27 @@
-import { defineConfig } from 'vite';
+import { ConfigEnv, UserConfigExport } from 'vite';
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import { name, version } from './package.json';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+export default ({ command, mode }: ConfigEnv): UserConfigExport => {
+  return {
+    plugins: [
+      vue(),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    base: command === 'build' && mode === 'cdn' ? `https://cdn.jianmu.run/${name}/${version}/` : '/',
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
     },
-  },
-  server: {
-    open: true,
-  },
-});
+    server: {
+      open: true,
+    },
+  };
+};
