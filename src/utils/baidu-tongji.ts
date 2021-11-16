0222b572b95c83f735b739fc047da7ef9a9cc71b
老技 <liamjung@live.com>
2021-11-16 04:32:24 +0000

    refactor: statistics

--- src/utils/baidu-tongji.ts
+++ src/utils/baidu-tongji.ts
@@ -2,7 +2,7 @@ if (import.meta.env.MODE === 'cdn') {
   console.debug('Baidu tongji installed');
   (function () {
     const hm = document.createElement('script');
-    hm.src = 'https://hm.baidu.com/hm.js?c5a1703dbc66ac55ad5439d39a96372c';
+    hm.src = 'https://hm.baidu.com/hm.js?b6b58635a8304396f110af0c2fe07d75';
     const s = document.getElementsByTagName('script')[0];
     s.parentNode?.insertBefore(hm, s);
   })();
